d93fd4f9974be6c97114a75a33045a796b2f65c4
Aarya <bhaveaarya09@gmail.com>
2024-01-16 21:11:43 +0530

    fixed_skew

--- Model_Construction/reorganizer.py
+++ Model_Construction/reorganizer.py
@@ -6,6 +6,7 @@ This code carries out the sorting and reorganizing of all the image data in '/da
 This code WILL DELETE '/data/all_images/' after creating the '/data/reorganized/' folder to save space.
 The '/data/reorganized/' folder will contain a proper sorted deep-copy of all the images from the original data pool.
 This file must be run with '/Model_Construction/' as current working directory.
+This file fixes the dataset skew and increases image count to ~42000 images.
 '''
 
 #IMPORT LIBRARIES :
@@ -14,6 +15,9 @@ import os
 import shutil
 import random
 import matplotlib.pyplot as plt
+import tensorflow as tf 
+import keras as K
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
 
 #Sort the images into a flow_from_dir capable folder structure
 #Create destination directory
@@ -29,6 +33,18 @@ train_dir = os.getcwd() + "/data/reorganized_train/"
 test_dir = os.getcwd() + "/data/reorganized_test/"
 validation_dir = os.getcwd() + "/data/reorganized_validation/"
 
+#Set up the image augmenter
+datagen = K.preprocessing.image.ImageDataGenerator(
+    rotation_range = 40,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    vertical_flip = True,
+    fill_mode = 'nearest'
+)
+
 #Load metadata
 metadata_frame = pd.read_csv('data/HAM10000_metadata.csv')
 
@@ -41,6 +57,17 @@ label_images = []
 
 #Keep counts
 test_examples = train_examples = validation_examples = 0
+
+#Create dictionary to balance weights
+balance_dict = {
+    'nv' : 0,
+    'mel' : 5,
+    'bkl' : 5,
+    'bcc' : 13,
+    'akiec' : 20,
+    'vasc' :  45,
+    'df' : 58
+}
 
 #Copy all the images to subfolders
 for i in label:
@@ -55,14 +82,44 @@ for i in label:
             #Add to training set.
             shutil.copyfile((data_dir+"/"+id+".jpg"), (train_dir+i+"/"+id+".jpg"))
             train_examples+=1
+            if(balance_dict[i]>0):
+                img = load_img(data_dir+"/"+id+".jpg")
+                x = img_to_array(img)
+                x = x.reshape((1,) + x.shape)
+                count = 0
+                for batch in datagen.flow(x, batch_size=1, save_to_dir=(train_dir+i), save_prefix='augmented', save_format='jpg'):
+                    count+=1
+                    train_examples+=1
+                    if(count>balance_dict[i]):
+                        break
         elif(random_num<0.9):
             #Add to crossvalidation set.
             shutil.copyfile((data_dir+"/"+id+".jpg"), (validation_dir+i+"/"+id+".jpg"))
             validation_examples+=1
+            if(balance_dict[i]>0):
+                img = load_img(data_dir+"/"+id+".jpg")
+                x = img_to_array(img)
+                x = x.reshape((1,) + x.shape)
+                count = 0
+                for batch in datagen.flow(x, batch_size=1, save_to_dir=(validation_dir+i), save_prefix='augmented', save_format='jpg'):
+                    count+=1
+                    validation_examples+=1
+                    if(count>balance_dict[i]):
+                        break
         else:
             #Add to testing set.
             shutil.copyfile((data_dir+"/"+id+".jpg"), (test_dir+i+"/"+id+".jpg"))
             test_examples+=1
+            if(balance_dict[i]>0):
+                img = load_img(data_dir+"/"+id+".jpg")
+                x = img_to_array(img)
+                x = x.reshape((1,) + x.shape)
+                count = 0
+                for batch in datagen.flow(x, batch_size=1, save_to_dir=(test_dir+i), save_prefix='augmented', save_format='jpg'):
+                    count+=1
+                    train_examples+=1
+                    if(count>balance_dict[i]):
+                        break
     label_images = []
 
 #Data visualization
